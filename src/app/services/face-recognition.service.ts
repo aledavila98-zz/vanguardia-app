38d4be357ed7e73bb5fcce79e4fc6e235bb4ebb0
CarlosVilla00896 <carlosvilla00896@gmail.com>
2020-03-14 02:34:40 -0600

    added face recognition service

--- src/app/services/face-recognition.service.ts
+++ src/app/services/face-recognition.service.ts
@@ -1,9 +1,62 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FaceRecognitionService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  scanImage(subscriptionKey: string, base64Image: string){
+    const headers = this.getHeaders(subscriptionKey);
+    const params = this.getParams();
+    const blob = this.makeblob(base64Image);
+
+    return this.httpClient.post<FaceRecognitionResponse>(
+      environment.endpoint,
+      blob,
+      {
+       params,
+       headers 
+      }
+      );
+  }
+
+  private makeblob(dataURL) {
+    const BASE64_MAKER = ';base64,';
+    const parts = dataURL.split(BASE64_MAKER  );
+    const contentType = parts[0].split(':')[1];
+
+    const raw = window.atob(parts[1]);
+    const rawLength = raw.length;
+    const uInt8Array = new Uint8Array(rawLength);
+
+    for (let i = 0; i < rawLength; ++i) {
+      uInt8Array[i] = raw.charCodeAt(i);
+    }
+
+    return new Blob([uInt8Array], { type: contentType })
+  }
+
+  private getHeaders(subscriptionKey: string) {
+    let headers = new HttpHeaders();
+    headers = headers.set('Content-Type', 'application/octet-stream');
+    headers = headers.set('Ocp-Apim-Subscription-Key', subscriptionKey);
+
+    return headers;
+  }
+
+  private getParams(){
+    const httParams = new HttpParams()
+    .set('returnFaceId', 'true')
+    .set('returnFaceLandMaks', 'false')
+    .set(
+      'returnFaceAttributes',
+      'age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise'
+    );
+    return httParams;
+  }
+
 }
